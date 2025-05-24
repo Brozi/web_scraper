013a578983f2e59290177fe77cd7c426d87340ca
Brozi <jasiubar@gmail.com>
2025-05-24 17:38:12 +0200

    feat code clean up

--- scraper.py
+++ scraper.py
@@ -1,52 +1,54 @@
 import requests as r
 from bs4 import BeautifulSoup
 import csv
-URL = "https://store.steampowered.com/app/1326470/Sons_Of_The_Forest/"
+while True:
+        url = input("Wpisz URL produktu w sklepie Steam, którego dane chcesz ściągnać: ").strip()
+        if (url == "") or ("https://" not in url) or ("store.steampowered.com/app" not in url):
+            print("\nPamiętaj o poprawnym adresie URL! Nie może być pusty, "
+                  "musi pochodzić ze sklepu Steam, oraz musi zaczynać się od 'https://'\n")
+        else:
+            break
+#print(f"Próbuję ściągnąć dane z {url}")
+#url = "https://store.steampowered.com/app/1326470/Sons_Of_The_Forest/"
 user_agent = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/136.0.0.0 Safari/537.36"
 }
-response = r.get(URL, headers=user_agent, timeout = 10)
-response.raise_for_status()
-html_content = response.text
-soup = BeautifulSoup(html_content, 'lxml')
-title_tag = soup.find('div', id='appHubAppName')
-title = "N/A"
-if title_tag is not None:
-    title = title_tag.get_text(strip=True)
-else:
-    print("Warning: Element not found")
-
-price_tag = soup.find('div', class_="game_purchase_price price")
-price = "N/A"
-if price_tag is not None:
-    price = price_tag.get_text(strip=True)
-else:
-    print("Warning: Element not found")
-
-release_tag = soup.find('div', class_="date")
-release = "N/A"
-if release_tag is not None:
-    release = release_tag.get_text(strip=True)
-else:
-    print("Warning: Element not found")
-
-reviews_tag = soup.find('span', class_="game_review_summary")
-reviews = "N/A"
-if reviews_tag is not None:
-    reviews = reviews_tag.get_text(strip=True)
-else:
-    print("Warning: Element not found")
 
 game_info = {
-    "Title" : title,
-    "Price" : price,
-    "Release date" : release,
-    "Reviews" : reviews,
+    "Title" : "",
+    "Price" : "",
+    "Release date" : "",
+    "Reviews" : "",
 }
 
-print(game_info)
+def none_check(element_tag):
+    element = "N/A"
+    if element_tag is not None:
+        element = element_tag.get_text(strip=True)
+    else:
+        print(f"Warning: Element {element_tag} not found")
+    return element
+
+response = r.get(url, headers=user_agent, timeout = 10)
+response.raise_for_status()
+
+html_content = response.text
+soup = BeautifulSoup(html_content, 'lxml')
+
+title_tag = soup.find('div', id='appHubAppName')
+price_tag = soup.find('div', class_="game_purchase_price price")
+release_tag = soup.find('div', class_="date")
+reviews_tag = soup.find('span', class_="game_review_summary")
+developers_tag = soup.find('div', id="developers_list")
+
+element_tags = [title_tag, price_tag, release_tag, reviews_tag, developers_tag]
+game = []
+
+for entry in element_tags:
+    game.append(none_check(entry))
+print(game)
 
 
 
