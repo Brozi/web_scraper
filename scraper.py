5382d3499516f1e9200c280917b3e5236252bcb8
Brozi <jasiubar@gmail.com>
2025-05-24 15:58:36 +0200

    feat add release, price and reviews scraping

--- scraper.py
+++ scraper.py
@@ -1,15 +1,41 @@
 import requests as r
 from bs4 import BeautifulSoup
-
-
 URL = "https://store.steampowered.com/app/1326470/Sons_Of_The_Forest/"
-response = r.get(URL, timeout = 10)
+user_agent = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/136.0.0.0 Safari/537.36"
+}
+response = r.get(URL, headers=user_agent, timeout = 10)
 response.raise_for_status()
 html_content = response.text
 soup = BeautifulSoup(html_content, 'lxml')
 title_tag = soup.find('div', id='appHubAppName')
+title = "N/A"
 if title_tag is not None:
     title = title_tag.get_text(strip=True)
+else:
+    print("Warning: Element not found")
+
+price_tag = soup.find('div', class_="game_purchase_price price")
+price = "N/A"
+if price_tag is not None:
+    price = price_tag.get_text(strip=True)
+else:
+    print("Warning: Element not found")
+
+release_tag = soup.find('div', class_="game_purchase_price price")
+price = "N/A"
+if price_tag is not None:
+    price = price_tag.get_text(strip=True)
+else:
+    print("Warning: Element not found")
+price_tag = soup.find('div', class_="game_purchase_price price")
+price = "N/A"
+if price_tag is not None:
+    price = price_tag.get_text(strip=True)
+else:
+    print("Warning: Element not found")
 
 
 
